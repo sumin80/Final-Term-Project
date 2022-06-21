99fefbbb10c93f169778dbf675555f72192723f2
sumin80 <yusumin71@gmail.com>
2022-06-21 20:16:40 +0900

    링크 오류

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -56,7 +56,7 @@ def question_delete(request, question_id):
 def question_vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.user == question.author:
-        messages.error(request, '본인이 작성한 글은 추천할수 없습니다')
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
     else:
         question.voter.add(request.user)
     return redirect('pybo:detail', question_id=question.id)
