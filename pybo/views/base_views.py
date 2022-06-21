ea60bfc9596d79b52240032a514a36d5e3a62541
sumin80 <yusumin71@gmail.com>
2022-06-21 18:47:14 +0900

    기말 프로젝트 커밋

---
+++ pybo/views/base_views.py
@@ -0,0 +1,34 @@
+import logging
+
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+
+from pybo.models import Question
+
+logger = logging.getLogger('pybo')
+
+
+def index(request):
+    logger.info("INFO 레벨로 출력")
+    page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')  
+    question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(answer__content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
